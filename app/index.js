e5529e2a1f6b6458e103a1fed09e1b73f09f7d83
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-16 17:06:46 +0100

    wip

--- app/index.js
+++ app/index.js
@@ -1,175 +1,164 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var GitHubApi = require('github');
-var cordova = require('cordova');
-var Q = require('q');
 
-var githubOptions = {
-  version: '3.0.0'
-};
-var github = new GitHubApi(githubOptions);
+var util = require('util'),
+    path = require('path'),
+    fs = require('fs'),
+    yeoman = require('yeoman-generator'),
+    GitHubApi = require('github'),
+    cordova = require('cordova'),
+    chalk = require('chalk'),
+    Q = require('q');
 
-var githubUserInfo = function (name, cb) {
-  github.user.getFrom({
-    user: name
-  }, function (err, res) {
-    if (err) {
-      throw err;
-    }
-    cb(JSON.parse(JSON.stringify(res)));
-  });
-};
+var prompts = require('./prompts');
 
 var MobileAppGenerator = module.exports = function MobileAppGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({
-        npm: true,
-        bower: false
+    var packagePath = path.join(__dirname, '../package.json'),
+        userSeedPath = path.join(process.env.HOME, '.seeds.json'),
+        generatorSeedPath = path.join(__dirname, '../conf/seeds.json');
+
+    this.seeds = [];
+    this.pkg = JSON.parse(this.readFileAsString(packagePath));
+
+    if(fs.existsSync(userSeedPath))
+        this.seeds = JSON.parse(this.readFileAsString(userSeedPath));
+
+    this.seeds = this.seeds.concat(JSON.parse(this.readFileAsString(generatorSeedPath)));
+
+    this.option('generator-version', {
+        alias: 'gv',
+        desc: 'Print the generator version',
+        required: 'false',
+        type: Boolean,
+        defaults: false
     });
-  });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-  this.seeds = JSON.parse(this.readFileAsString(path.join(__dirname, '../conf/seeds.json')));
+    this.option('list', {
+        alias: 'l',
+        desc: 'List available seeds',
+        required: 'false',
+        type: Boolean,
+        defaults: false
+    });
+
+    if(options.l || options.list) {
+        this.seeds.forEach(function (seed) {
+            console.log(chalk.blue(seed.id) + " v" + seed.version + " ➜ " + seed.uri);
+        }, this);
+        process.exit(0);
+    }
+
+    if(options.gv || options['generator-version']) {
+        console.log(this.pkg.version);
+        process.exit(0);
+    }
+
+    this.on('end', function () {
+        this.installDependencies({
+            npm: true,
+            bower: false
+        });
+    });
+
 };
 
 util.inherits(MobileAppGenerator, yeoman.generators.Base);
 
+var githubOptions = { version: '3.0.0' },
+    github = new GitHubApi(githubOptions),
+    githubUserInfo = function (name, cb) {
+        github.user.getFrom({
+            user: name
+        }, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(JSON.parse(JSON.stringify(res)));
+        });
+    };
+
 MobileAppGenerator.prototype.askUserFor = function askFor() {
-  var cb = this.async();
+    var done = this.async();
 
-  var prompts = [{
-    type:'input',
-    name:'githubUser',
-    message:'What is your github username?',
-    default:null
-  },{
-    type:'input',
-    name:'appName',
-    message:'What is the name of the mobile App?',
-    default: path.basename(process.cwd())
-  },{
-    type:'input',
-    name:'appId',
-    message:'What is the ID of the mobile App\n (reverse-domain-style package name, like com.foo.myapp)?',
-    validate: function (input) {
-        return input.match(/(^[a-z]{1}[a-z0-9]+\.[a-z]{1}[a-z0-9]+\.[a-z]{1}[a-z0-9]+$)/g) ? true : "Need an Id like com.foo.myapp!";
-    }
-  },
-  {
-    type:'input',
-    name:'appDescription',
-    message:'Can you give a description of your mobile App?',
-    default: ""
-  },
-  {
-    type:'checkbox',
-    name:'targets',
-    choices:[{
-        name:'ios',
-        value:'ios'
-    },{
-        name:'Android',
-        value:'android'
-    }],
-    message:'Which targets do you want to support ?',
-    validate: function (input) {
-        return input instanceof Array && input.length > 0 ? true : "One minimum target is required";
-    }
-  },
-  {
-    type:'checkbox',
-    name:'plugins',
-    choices: JSON.parse(this.readFileAsString(path.join(__dirname, '../conf/plugins.json'))),
-    message:'With which Cordova official plugins to you want to start?'
-  },
-  {
-    type:'list',
-    name:'seed',
-    choices: this.seeds.map(function (seed) { return seed.id ; }),
-    message:'With which project seed do you want to start with?'
-  }];
-
-  this.prompt(prompts, function (props) {
-    this.githubUser = props.githubUser;
-    this.appName = props.appName;
-    this.appId = props.appId;
-    this.appDescription = props.appDescription;
-    this.targets = props.targets;
-    this.plugins = props.plugins;
-    this.seed = props.seed;
-    cb();
-  }.bind(this));
-
+    this.prompt(prompts.call(this), function (props) {
+        this.githubUser = props.githubUser;
+        this.appName = props.appName;
+        this.appId = props.appId;
+        this.appDescription = props.appDescription;
+        this.targets = props.targets;
+        this.plugins = props.plugins;
+        this.seed = props.seed;
+        done();
+    }.bind(this));
 };
 
 MobileAppGenerator.prototype.userInfo = function userInfo() {
-  var done = this.async();
+    var done = this.async();
 
-  githubUserInfo(this.githubUser, function (res, err) {
-    if (err) {
-        this.realname = null;
-        this.email = null;
-        this.githubUrl = null;
+    githubUserInfo(this.githubUser, function (res, err) {
+        if (err) {
+            this.realname = null;
+            this.email = null;
+            this.githubUrl = null;
+            done();
+        }
+        this.realname = res.name;
+        this.email = res.email;
+        this.githubUrl = res.html_url;
         done();
-    }
-    this.realname = res.name;
-    this.email = res.email;
-    this.githubUrl = res.html_url;
-    done();
-  }.bind(this));
+    }.bind(this));
 };
 
 MobileAppGenerator.prototype.initCordova = function initCordova() {
-  var cb = this.async(),
-      self = this,
-      cfg = { lib : { www : this.seeds.filter(function (seed) {
-        return seed.id === self.seed;
-      })[0] } };
+    var cb = this.async(),
+        self = this,
+        cfg = { lib : { www : {
+            id : "empty",
+            version : "0.0.0",
+            uri : path.join(__dirname, '../conf/empty-www')
+        } } };
 
-  cordova.create('.', this.appId, this.appName, cfg, function () {
-    this.log.write().ok('Raw app created');
-    cb();
-  }.bind(this));
+    cordova.create('.', this.appId, this.appName, cfg, function () {
+        this.log.write().ok('Raw app created');
+        cb();
+    }.bind(this));
 };
 
 MobileAppGenerator.prototype.target = function target() {
-  var cb = this.async();
+    var cb = this.async();
 
-  cordova.platform('add', this.targets, function () {
-    this.log.ok('Targets added: ' + this.targets.join(', ')).write();
-    cb();
-  }.bind(this));
+    cordova.platform('add', this.targets, function () {
+        this.log.ok('Targets added: ' + this.targets.join(', ')).write();
+        cb();
+    }.bind(this));
 };
 
 MobileAppGenerator.prototype.cordovaplugins = function cordovaplugins() {
-  var cb = this.async();
+    var cb = this.async();
 
-  cordova.plugins('add', this.plugins, function () {
-    this.plugins.forEach(function (plugin) {
-        this.log.ok('Added plugin: ' + plugin).write();
+    cordova.plugins('add', this.plugins, function () {
+        this.plugins.forEach(function (plugin) {
+            this.log.ok('Added plugin: ' + plugin).write();
+        }.bind(this));
+        cb();
     }.bind(this));
-    cb();
-  }.bind(this));
 };
 
-/*MobileAppGenerator.prototype.seedAskUserFor = function seedAskUserFor() {
-    this.log.ok('seed should ask user for complementary informations').write();
-};*/
+MobileAppGenerator.prototype.execSeedGenerator = function oops() {
+    var cb = this.async();
+    yeoman('default-seed', {
+        userSettings : {
+            "appName" : this.appName
+        }
+    }).register('generator-default-seed', 'default-seed').run(function (err) {
+        if (err) this.log.error(err).write();
+        cb();
+    });
+};
 
 MobileAppGenerator.prototype.app = function app() {
-  this.template('_package.json', 'package.json');
-  this.template('_README.md', 'README.md');
-  this.copy('gitignore', '.gitignore');
-};
-
-MobileAppGenerator.prototype.oops = function oops() {
- var cb = this.async();
-  yeoman('default-seed').register('generator-default-seed', 'default-seed').run(function (err) {
-    console.log(err, 'OOOOOPS');
-    cb();
-  });
+    this.template('_package.json', 'package.json');
+    this.template('_README.md', 'README.md');
+    this.copy('gitignore', '.gitignore');
 };
